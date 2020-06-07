e7161af25daf21c6d922e05f9840b99894159ddd
agdsarge <agdsargeant@gmail.com>
2020-06-07 17:41:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+//my comment
+
+function submitData(name, email) {
+        let userData = {
+            name: name,
+            email: email
+        }
+        return fetch("http://localhost:3000/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(userData)
+        })
+        .then( (resp) => resp.json() )
+        .then( (usr) => {
+            // let div = document.createElement("div");
+            // div.innerText = usr.id;
+            document.body.innerHTML = usr.id; // Learn wants innerHTML, not innerText.
+        })
+        .catch( (error) => {
+            // let div = document.createElement("div");
+            // div.innerText = error.message;
+            document.body.innerHTML = error.message; // Learn wants innerHTML, not innerText.
+        })
+
+}
